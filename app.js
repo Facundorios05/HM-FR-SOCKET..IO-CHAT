ac1b63493c82ec26c9a498e527973bcbaabbdb1e
Meyer <meyer-hector@hotmail.com>
2023-11-08 11:31:56 -0300

    Feat: se habilito la relacion de usuario-mensaje

--- app.js
+++ app.js
@@ -11,30 +11,38 @@ const io = new Server(server);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
-let messages = []
+let user
+let message
+let messages = [
+]
 io.on('connection', (socket) => {
-    console.log(socket.id);
 
-    socket.on('chat message', (msg) => {
-        socket.broadcast.emit(msg);
+    socket.on('new-user', (username) => {
+        user = username
+        console.log('Nuevo usuario conectado:', username);
+    })
 
-        messages.push(msg);
-        io.emit('chat message', msg);
-        console.log(messages)
+    socket.on('chat message', (data) => {
+        let user = data.user
+        let msg = data.message
+        socket.broadcast.emit(user, msg);
+        io.emit('chat message', { user, msg });
+        message = msg
+        messages.push({ user, message })
     });
-
-
 
     socket.on('disconnect', () => {
         console.log('user disconnected', socket.id);
     });
 
 })
-app.get('/api/messages', () => {
-    return messages;
+app.get('/api/messages', (req, res) => {
+    res.json(messages)
 })
 
 server.listen(3000, () => {
